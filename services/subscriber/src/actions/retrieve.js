48e8a1d44c05b79ec17cf2d9450d311a76ff127c
Jacob Bare <jacob@limit0.io>
2018-11-29 17:26:51 -0600

    Move actions into own files

---
+++ services/subscriber/src/actions/retrieve.js
@@ -0,0 +1,25 @@
+const { createError } = require('micro');
+const { invalidParamMsg } = require('@limit-zero/lm-common');
+
+const { isArray } = Array;
+
+module.exports = async ({ by, value, props }, { mc }) => {
+  const supported = ['ID', 'SubscriberKey'];
+  if (!supported.includes(by)) throw createError(400, invalidParamMsg('params.by', by, supported));
+  try {
+    const subscriber = await mc.retrieveOne('Subscriber', {
+      attributes: { 'xsi:type': 'SimpleFilterPart' },
+      Property: by,
+      SimpleOperator: 'equals',
+      Value: value,
+    }, isArray(props) ? props : undefined);
+    if (subscriber) delete subscriber.attributes;
+    return { data: subscriber };
+  } catch (e) {
+    if (/^The Request Property/.test(e.message)) {
+      // Send a 400 on bad props.
+      e.statusCode = 400;
+    }
+    throw e;
+  }
+};
