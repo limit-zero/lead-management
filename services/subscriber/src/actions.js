5702d4b07bc4f23c192700473b60eedd79dbb869
Jacob Bare <jacob@limit0.io>
2018-11-29 17:23:01 -0600

    Send 400 on bad props

--- services/subscriber/src/actions.js
+++ services/subscriber/src/actions.js
@@ -7,13 +7,20 @@ module.exports = {
   retrieve: async ({ by, value, props }, { mc }) => {
     const supported = ['ID', 'SubscriberKey'];
     if (!supported.includes(by)) throw createError(400, invalidParamMsg('params.by', by, supported));
-    const subscriber = await mc.retrieveOne('Subscriber', {
-      attributes: { 'xsi:type': 'SimpleFilterPart' },
-      Property: by,
-      SimpleOperator: 'equals',
-      Value: value,
-    }, isArray(props) ? props : undefined);
-
-    return { data: subscriber };
+    try {
+      const subscriber = await mc.retrieveOne('Subscriber', {
+        attributes: { 'xsi:type': 'SimpleFilterPart' },
+        Property: by,
+        SimpleOperator: 'equals',
+        Value: value,
+      }, isArray(props) ? props : undefined);
+      return { data: subscriber };
+    } catch (e) {
+      if (/^The Request Property/.test(e.message)) {
+        // Send a 400 on bad props.
+        e.statusCode = 400;
+      }
+      throw e;
+    }
   },
 };
