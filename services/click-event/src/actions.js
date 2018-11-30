58c3f2e2d0bf2a11fd779a1e6838c4b1e27cd741
Jacob Bare <jacob@limit0.io>
2018-11-30 11:22:02 -0600

    Use micro client call in test action

--- services/click-event/src/actions.js
+++ services/click-event/src/actions.js
@@ -1,22 +1,13 @@
-const fetch = require('node-fetch');
+const call = require('@limit-zero/lm-micro-client');
 
 module.exports = {
   test: async () => {
-    // @todo This should be abstracted.
-    const res = await fetch('http://subscriber', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json; charset=UTF-8' },
-      body: JSON.stringify({
-        action: 'retrieve',
-        params: {
-          by: 'ID',
-          value: '2300856',
-          props: ['ID', 'EmailAddress'],
-        },
-      }),
-    });
-    // @todo Errors need to be JSON, otherwise this will fail.
-    const json = await res.json();
+    const params = {
+      by: 'ID',
+      value: '2300856',
+      props: ['ID', 'EmailAddress'],
+    };
+    const json = await call('subscriber.retrieve', { params });
     return { ID: json.data.ID };
   },
 };
