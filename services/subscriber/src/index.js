341d4be43892c956f12a1ca014d8367e8e8b4562
Jacob Bare <jacob@limit0.io>
2018-11-29 19:56:11 -0600

    Create json error handler

--- services/subscriber/src/index.js
+++ services/subscriber/src/index.js
@@ -1,8 +1,25 @@
+const { send } = require('micro');
 const { jsonService } = require('@limit-zero/lm-micro-service');
 const mc = require('@limit-zero/lm-marketing-cloud');
 const actions = require('./actions');
 
-module.exports = async (req, res) => {
+// @todo This should be handled by the json service.
+const DEV = process.env.NODE_ENV === 'development';
+const handleErrors = fn => async (req, res) => {
+  try {
+    return await fn(req, res);
+  } catch (e) {
+    const statusCode = e.statusCode || e.status;
+    const obj = { error: true, message: e.message };
+    if (DEV) obj.stack = e.stack.split('\n');
+    send(res, statusCode || 500, obj);
+    // eslint-disable-next-line no-console
+    if (e instanceof Error) console.error(e.stack);
+    // return null;
+  }
+};
+
+module.exports = handleErrors(async (req, res) => {
   await mc.client();
   return jsonService({
     actions,
@@ -10,4 +27,4 @@ module.exports = async (req, res) => {
     res,
     ctx: { mc },
   });
-};
+});
