256368ba1c064ef5f521dcad46e665d0853eecac
Jacob Bare <jacob@limit0.io>
2018-11-30 11:39:31 -0600

    Create mc-send service

---
+++ services/mc-send/src/actions/retrieve.js
@@ -0,0 +1,26 @@
+const { createError } = require('micro');
+const { createParamError } = require('@limit-zero/lm-micro-service');
+
+const { isArray } = Array;
+
+module.exports = async ({ by, value, props }, { mc }) => {
+  const supported = ['ID'];
+  if (!supported.includes(by)) throw createParamError('params.by', by, supported);
+  try {
+    const send = await mc.retrieveOne('Send', {
+      attributes: { 'xsi:type': 'SimpleFilterPart' },
+      Property: by,
+      SimpleOperator: 'equals',
+      Value: value,
+    }, isArray(props) ? props : undefined);
+    if (!send) throw createError(404, `No Send found for ${by} ${value}`);
+    if (send) delete send.attributes;
+    return { data: send };
+  } catch (e) {
+    if (/^The Request Property/.test(e.message)) {
+      // Send a 400 on bad props.
+      e.statusCode = 400;
+    }
+    throw e;
+  }
+};
