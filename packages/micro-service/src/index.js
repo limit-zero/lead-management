e0fa06e6223511c5159bec4c9c0bbb497e3b81d9
Jacob Bare <jacob@limit0.io>
2018-11-29 18:24:35 -0600

    Move json service into own file

--- packages/micro-service/src/index.js
+++ packages/micro-service/src/index.js
@@ -1,29 +1,5 @@
-const { json, createError } = require('micro');
+const jsonService = require('./json-service');
 const createParamError = require('./param-error');
-
-const jsonService = async ({
-  actions = {},
-  req,
-  res,
-  ctx = {},
-} = {}) => {
-  const { url } = req;
-  if (url === '/_status') return 'OK';
-  const input = await json(req);
-  const { action, params, meta } = input;
-  if (!action) throw createError(400, 'No action provided.');
-
-  const fn = actions[action];
-  if (typeof fn !== 'function') throw createParamError('action', action, Object.keys(actions));
-
-  const output = await fn(params || {}, {
-    req,
-    res,
-    meta: meta || {},
-    ...ctx,
-  });
-  return output || {};
-};
 
 module.exports = {
   jsonService,
---
+++ packages/micro-service/src/json-service.js
@@ -0,0 +1,26 @@
+const { json, createError } = require('micro');
+const createParamError = require('./param-error');
+
+module.exports = async ({
+  actions = {},
+  req,
+  res,
+  ctx = {},
+} = {}) => {
+  const { url } = req;
+  if (url === '/_status') return 'OK';
+  const input = await json(req);
+  const { action, params, meta } = input;
+  if (!action) throw createError(400, 'No action provided.');
+
+  const fn = actions[action];
+  if (typeof fn !== 'function') throw createParamError('action', action, Object.keys(actions));
+
+  const output = await fn(params || {}, {
+    req,
+    res,
+    meta: meta || {},
+    ...ctx,
+  });
+  return output || {};
+};
