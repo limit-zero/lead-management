9717f8d0828be41fa38783aca62d36388a3f4ab3
Jacob Bare <jacob@limit0.io>
2018-12-02 12:06:59 -0600

    Create mc-link-send service

---
+++ services/mc-link-send/src/actions/retrieve-for-send.js
@@ -0,0 +1,21 @@
+const { createError } = require('micro');
+
+module.exports = async ({ sendId, props }, { mc }) => {
+  if (!sendId) throw createError(400, "No 'sendId' was provided.");
+  try {
+    const Filter = {
+      attributes: { 'xsi:type': 'SimpleFilterPart' },
+      Property: 'SendID',
+      SimpleOperator: 'equals',
+      Value: sendId,
+    };
+    const { Results } = await mc.retrieve('LinkSend', props, { Filter });
+    return Results;
+  } catch (e) {
+    if (/^The Request Property/.test(e.message)) {
+      // Send a 400 on bad props.
+      e.statusCode = 400;
+    }
+    throw e;
+  }
+};
