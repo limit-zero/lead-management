443aada01225c9b89ad223765385b838b3d33f96
Jacob Bare <jacob@limit0.io>
2018-12-09 14:43:37 -0600

    Create url hash action

--- services/url/src/actions/index.js
+++ services/url/src/actions/index.js
@@ -1,11 +1,13 @@
 const clean = require('./clean');
 const crawl = require('./crawl');
 const extractFromHtml = require('./extract-from-html');
+const hash = require('./hash');
 const mapFromHtml = require('./map-from-html');
 
 module.exports = {
   clean,
   crawl,
   'extract-from-html': extractFromHtml,
+  hash,
   'map-from-html': mapFromHtml,
 };
