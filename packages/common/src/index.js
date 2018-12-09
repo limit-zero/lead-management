ef2237009568287d88d4d566470e0955c746fff9
Jacob Bare <jacob@limit0.io>
2018-12-09 09:53:39 -0600

    Create elapsed time util

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -1,7 +1,9 @@
+const elapsed = require('./elapsed');
 const formatEmail = require('./format-email');
 const isObject = require('./is-object');
 
 module.exports = {
+  elapsed,
   formatEmail,
   isObject,
 };
