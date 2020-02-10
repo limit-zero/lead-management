3ef01b1f438f2e8721f9ee32ec50a5609e969235
Jacob Bare <jacob@limit0.io>
2020-02-09 20:18:02 -0600

    Require the url parameter when hashing

--- services/url/src/actions/hash.js
+++ services/url/src/actions/hash.js
@@ -1,6 +1,8 @@
 const { createHash } = require('crypto');
+const { createRequiredParamError } = require('@base-cms/micro').service;
 
-module.exports = async ({ url }) => {
+module.exports = async ({ url } = {}) => {
+  if (!url) throw createRequiredParamError('url');
   const hash = createHash('md5').update(url ? `${url}` : '').digest('hex');
   return { hash };
 };
