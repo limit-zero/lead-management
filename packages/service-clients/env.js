4214239936483b0607499d31e16d899a91726fb7
Jacob Bare <jacob@limit0.io>
2020-02-09 19:46:33 -0600

    Create service clients pkg

---
+++ packages/service-clients/env.js
@@ -0,0 +1,15 @@
+const {
+  cleanEnv,
+  validators,
+} = require('@base-cms/env');
+
+const { nonemptystr } = validators;
+
+const services = [
+];
+
+module.exports = cleanEnv(process.env, services.reduce((o, name) => {
+  const prop = `${name.toUpperCase()}_SERVICE_URL`;
+  const validator = nonemptystr({ desc: `The lead management ${name} service URL to connect to.`, default: `http://${name}` });
+  return { ...o, [prop]: validator };
+}, {}));
