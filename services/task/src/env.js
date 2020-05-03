cb5b9fc393cbcbf480524b1b736c21b9d9c6f164
Jacob Bare <jacob@limit0.io>
2020-05-03 17:07:38 -0500

    Add grapqhl client

--- services/task/src/env.js
+++ services/task/src/env.js
@@ -5,6 +5,7 @@ const {
 } = require('envalid');
 
 module.exports = cleanEnv(process.env, {
+  MC_GRAPHQL_URI: str({ desc: 'The Marketing Cloud GraphQL server to connect to.' }),
   MONGO_URI: str({ desc: 'The MongoDB database to connect to.' }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: str({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
