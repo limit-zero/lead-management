082fda3a674604aa655caf95c059eec39564f771
Jacob Bare <jacob@limit0.io>
2020-05-04 07:39:34 -0500

    Add find action

--- services/queue/src/actions/index.js
+++ services/queue/src/actions/index.js
@@ -1,6 +1,8 @@
 const { checkArray, checkRequired } = require('@lead-management/micro/utils');
 const mongodb = require('../mongodb');
 
+const getCollection = () => mongodb.collection('leads-graph', 'mc-upsert-queue');
+
 const createOp = ({ namespace, identifier }) => {
   checkRequired('namespace', namespace);
   checkRequired('identifier', identifier);
@@ -18,11 +20,26 @@ const createOp = ({ namespace, identifier }) => {
 const bulkAdd = async ({ items } = {}) => {
   checkArray('items', items);
   const bulkOps = items.map(createOp);
-  const coll = await mongodb.collection('leads-graph', 'mc-upsert-queue');
+  const coll = await getCollection();
   return coll.bulkWrite(bulkOps);
 };
 
 module.exports = {
+  /**
+   *
+   */
+  find: async ({ query = {}, options = {} } = {}) => {
+    const coll = await getCollection();
+    const defaultOptions = { limit: 50, sort: { last: 1 } };
+    return coll.find(query, { ...defaultOptions, ...options }).toArray();
+  },
+  /**
+   *
+   */
   add: ({ namespace, identifier } = {}) => bulkAdd({ items: [{ namespace, identifier }] }),
+
+  /**
+   *
+   */
   bulkAdd,
 };
