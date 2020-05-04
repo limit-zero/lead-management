e6abd9ea2edc653062595fbe3f07b3b1b1595739
Jacob Bare <jacob@limit0.io>
2020-05-04 11:55:51 -0500

    Delete unsued graphql factory package

--- packages/graphql-client-factory/src/env.js
+++
@@ -1,8 +0,0 @@
-const {
-  cleanEnv,
-  str,
-} = require('envalid');
-
-module.exports = cleanEnv(process.env, {
-  MC_GRAPHQL_URI: str({ desc: 'The primary Marketing Cloud GraphQL API to connect to.' }),
-});
--- packages/graphql-client-factory/src/index.js
+++
@@ -1,6 +0,0 @@
-const { createClient } = require('@lead-management/mc-graphql-client');
-const { MC_GRAPHQL_URI } = require('./env');
-
-module.exports = () => ({
-  primary: createClient({ uri: MC_GRAPHQL_URI }),
-});
