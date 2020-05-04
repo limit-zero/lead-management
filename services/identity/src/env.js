32a78e211de2cefcc3d4e977da0cdb8a8fe43c81
Jacob Bare <jacob@limit0.io>
2020-05-04 07:49:21 -0500

    Add task, clients, and graphql deps

--- services/identity/src/env.js
+++ services/identity/src/env.js
@@ -8,6 +8,7 @@ const {
 module.exports = cleanEnv(process.env, {
   EXTERNAL_PORT: port({ desc: 'The external port that the service is exposed on.', default: 80 }),
   INTERNAL_PORT: port({ desc: 'The internal port that the service will run on.', default: 80 }),
+  MC_GRAPHQL_URI: str({ desc: 'The Marketing Cloud GraphQL server to connect to.' }),
   MONGO_URI: str({ desc: 'The MongoDB database to connect to.' }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: str({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
