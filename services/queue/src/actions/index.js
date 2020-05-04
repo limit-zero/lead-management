58c1099300e06cd3ad920af5c128dc3b4ea7ea4f
Jacob Bare <jacob@limit0.io>
2020-05-03 20:01:53 -0500

    Add bulkAdd and add actions

--- services/queue/src/actions/index.js
+++ services/queue/src/actions/index.js
@@ -1,1 +1,28 @@
-module.exports = {};
+const { checkArray, checkRequired } = require('@lead-management/micro/utils');
+const mongodb = require('../mongodb');
+
+const createOp = ({ namespace, identifier }) => {
+  checkRequired('namespace', namespace);
+  checkRequired('identifier', identifier);
+
+  const criteria = { namespace: `${namespace}`, identifier: `${identifier}` };
+  return {
+    updateOne: {
+      filter: criteria,
+      update: { $setOnInsert: criteria },
+      upsert: true,
+    },
+  };
+};
+
+const bulkAdd = async ({ items } = {}) => {
+  checkArray('items', items);
+  const bulkOps = items.map(createOp);
+  const coll = await mongodb.collection('leads-graph', 'mc-upsert-queue');
+  return coll.bulkWrite(bulkOps);
+};
+
+module.exports = {
+  add: ({ namespace, identifier } = {}) => bulkAdd({ items: [{ namespace, identifier }] }),
+  bulkAdd,
+};
